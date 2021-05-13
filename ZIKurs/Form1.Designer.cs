7dc1b0e872ab44b91e7384ada49d3266b5492da4
Titamik <CardoorOleg@yandex.ru>
2021-05-13 20:08:45 +0300

    Add files via upload

---
+++ ZIKurs/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace ZIKurs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Проверка подключения к Интернету";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Проверка наличия установленного межсетевого экрана";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(244, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Проверка работоспособности межсетевого экрана";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(262, 74);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(526, 23);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(262, 103);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(526, 41);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(262, 150);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(526, 41);
+            this.richTextBox3.TabIndex = 7;
+            this.richTextBox3.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(334, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Межсетевой экран";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(366, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Антивирус";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(262, 339);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(526, 41);
+            this.richTextBox4.TabIndex = 15;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(262, 292);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(526, 41);
+            this.richTextBox5.TabIndex = 14;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(262, 263);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(526, 23);
+            this.richTextBox6.TabIndex = 13;
+            this.richTextBox6.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 339);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(244, 41);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Тестирование антивирусного ПО";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 292);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(244, 41);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Проверка работоспособности антивирусного ПО";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 263);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(244, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Проверка наличия антивируса";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 424);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 199);
+            this.listBox1.TabIndex = 16;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(620, 424);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(168, 23);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "Результаты";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_2);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(620, 453);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(168, 23);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "В файл";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(620, 482);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(168, 23);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "Очистить";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(227, 424);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(387, 199);
+            this.richTextBox7.TabIndex = 21;
+            this.richTextBox7.Text = "";
+            this.richTextBox7.TextChanged += new System.EventHandler(this.richTextBox7_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 638);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        public System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.Button button7;
+        public System.Windows.Forms.Button button8;
+        public System.Windows.Forms.Button button9;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ZIKurs/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Security.Permissions;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ZIKurs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPStatus status = IPStatus.Unknown;
+            try
+            {
+                status = new Ping().Send("www.google.ru").Status;
+            }
+            catch { }
+
+            if (status == IPStatus.Success)
+            {
+                richTextBox1.Text = "Данный компьютер подключен к интернету";
+
+            }
+            else
+            {
+                richTextBox1.Text = "Данный компьютер не подключен к интернету";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!(File.Exists((@"c:\windows\regedit.exe"))))
+            {
+                richTextBox2.Text = "Фаервол WF.msc установлен!";
+            }
+            else
+            {
+                richTextBox2.Text = "Фаервол WF.msc не установлен!";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            WebClient Client = new WebClient();
+            String Response;
+            try
+            {
+                Response = Client.DownloadString("https://www.google.com");
+            }
+            catch
+            {
+                richTextBox3.Text = "Межсетевой экран функционирует правильно!";
+            }
+            if (richTextBox3.Text == "")
+            {
+                richTextBox3.Text = "Межсетевой экран функционирует неверно, или не функционирует вовсе!";
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var runningProcs = from proc in Process.GetProcesses(".") orderby proc.Id select proc;
+            if (runningProcs.Count(p => p.ProcessName.Contains("AvastSvc")) > 0)
+            {
+                richTextBox5.Text = "Антивирус Avast Free работает!";
+            }
+            else
+            {
+                richTextBox5.Text = "Антивирус Avast Free не работает!";
+            }
+        }
+
+            private void GetInstalledSoftware()
+        {
+            List<string> items = new List<string>();
+            using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))  // или 32
+            using (var rk = hklm.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
+            {
+                foreach (string skName in rk.GetSubKeyNames())
+                {
+                    using (RegistryKey sk = rk.OpenSubKey(skName))
+                    {
+                        try
+                        {
+                            if (sk.GetValue("DisplayName") != null)
+                            {
+                                items.Add(sk.GetValue("DisplayName").ToString());
+                                listBox1.Items.Add(new ListViewItem(items.ToArray()));
+                                listBox1.SetSelected(listBox1.Items.Count - 1, true);
+                                if (listBox1.SelectedItem.ToString() == "ListViewItem: {Avast Free Antivirus}")
+                                {
+                                    richTextBox6.Text = "Антивирус Avast Free установлен в системе";
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+
+                    items.Clear();
+                }
+            }
+        }
+        private string CheckValue(object input)
+        {
+            if (input != null)
+                return input.ToString();
+            else
+                return string.Empty;
+        }
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            GetInstalledSoftware();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox4.Text = "Ожидаем..";
+            string file_name = "eicar.com";
+            File.WriteAllText("eicar.com", "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H" + "+H*");
+            Thread.Sleep(35000);
+            if (!File.Exists(file_name))
+            {
+                richTextBox4.Text = "Антивирус Avast Free прошел проверку";
+            } else
+            {
+                richTextBox4.Text = "Антивирус Avast Free НЕ прошел проверку";
+            }
+
+        }
+
+        private void button7_Click_2(object sender, EventArgs e)
+        {
+
+            richTextBox7.Text = "Ожидаем..";
+
+            button1_Click(button1, null);
+            button2_Click(button2, null);
+            button3_Click(button3, null);
+            button4_Click(button4, null);
+            button5_Click(button5, null);
+            button6_Click(button6, null);
+
+            richTextBox7.Clear();
+
+            richTextBox7.Text = richTextBox1.Text + System.Environment.NewLine + richTextBox2.Text + System.Environment.NewLine + richTextBox3.Text + System.Environment.NewLine + richTextBox4.Text + System.Environment.NewLine + richTextBox5.Text + System.Environment.NewLine + richTextBox6.Text;
+
+            richTextBox1.Clear(); richTextBox2.Clear();
+            richTextBox3.Clear(); richTextBox4.Clear();
+            richTextBox5.Clear(); richTextBox6.Clear();
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = saveFileDialog1.FileName;
+                // сохраняем текст в файл
+                File.WriteAllText(filename, richTextBox7.Text);
+
+
+            }
+        }
+            private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
